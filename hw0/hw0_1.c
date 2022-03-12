f41ea346839937783e86e3059a6f312efdd6d090
bHead_96 <r10525103@ntu.edu.tw>
2022-03-12 13:38:32 +0800

    Add divisorv fucntion

--- hw0/hw0_1.c
+++ hw0/hw0_1.c
@@ -10,6 +10,8 @@ int *strToInt(char *);
 void print_the_number(int *);
 void swap(int **, int **);
 void substractor(int **num1, int **num2);
+char *get_num_in_str();
+void divisor(int **);
 /*
 void strToInt(char toDo[], int arr_len, int result[])
 
@@ -20,61 +22,20 @@ szince char is different to char array, you should type char* instead of char in
 */
 
 
-char * get_num_in_str(void) {
-    char * str_num = (char *) malloc((MAX_LENGTH + 1) * sizeof(char));
-    printf("Please type in number \n");
-    scanf("%s", str_num);
-    return str_num;
-}
-
-
 int main(void){
-/*     char num1[256];
-    char num2[256]; */
-/*     printf("Please type in number 1\n");
-    scanf("%s", num1);
-    printf("%s", num1);
-    printf("Please type in numebr 2\n");
-    scanf("%s", num2);
-    printf("%s", num2); */
-
+    
     char *num1 = get_num_in_str();    
     char *num2 = get_num_in_str();    
     
-    int *num1_transform = strToInt(num1);  
+    int *num1_transform = strToInt(num1);
+    int *num2_transform = strToInt(num2);      
 
-    int *num2_transform = strToInt(num2);  
-    
-        
-    //print_the_number(num1_transform);
-    //printf("%d\n", comparator(num1_transform, num2_transform));
+    printf("%d\n", comparator(num1_transform, num2_transform));
     substractor(&num1_transform, &num2_transform);
-    print_the_number(num1_transform);
+    
+    
 }
 
-int comparator(int *num1, int *num2)
-//not finished yet, you can only compare the length
-{
-    if (num1[0] > num2[0])
-        return true;
-    else{
-        if (num1[0] < num2[0])
-            return false;
-        else{
-            //compare every number of them
-            for (int i = num1[0]; i > 0; i--){
-                if (num1[i] > num2[i]){
-                    return true;
-                }
-                else if (num1[i] < num2[i]){
-                    return false;
-                }
-            }
-            return 2;
-            //return 2 if they are equl
-        }    
-    }
-}
 
 void swap(int **num1, int **num2)
 {
@@ -82,36 +43,6 @@ void swap(int **num1, int **num2)
     num1 = num2;
     num2 = tmp;
 }
-
-
-/* void substractor(int **num1, int **num2)
-{
-    int num1_len = *num1[0];
-    int num2_len = *num2[0];
-    //printf("%d\n", num1_len);
-    for (int i = 1; i <= num2_len; i++) {
-    //You can't use num1_len as a boundary since you have no idea about the element of 
-    //num2[num1_len] if their's length are not equal. It might result in a segmentation fault.
-        if (*num1[i] >= *num2[i]) {
-            *num1[i] = (*num1[i]) - (*num2[i]);
-        }
-        else {
-            *num1[i] = (*num1[i]) + 10;
-            int j = i;
-            while (*num1[j+1] == 0) {
-                j++;
-                *num1[j] = 9;
-            }
-            (*num1[j+1])--;                    
-            *num1[i] = (*num1[i])- (*num2[i]);
-        }
-    }
-    //Check if the length of num1 is still the same.
-    while (*num1[num1_len] == 0){
-        *num1[0] = (*num1[0]) - 1;
-        num1_len--;
-    }
-} */
 
 
 void substractor(int **num1, int **num2)
@@ -143,8 +74,52 @@ void substractor(int **num1, int **num2)
     }
 }
 
-//divisor
+int comparator(int *num1, int *num2)
+//not finished yet, you can only compare the length
+{
+    if (num1[0] > num2[0])
+        return true;
+    else{
+        if (num1[0] < num2[0])
+            return false;
+        else{
+            //compare every number of them
+            for (int i = num1[0]; i > 0; i--){
+                if (num1[i] > num2[i]){
+                    return true;
+                }
+                else if (num1[i] < num2[i]){
+                    return false;
+                }
+            }
+            return 2;
+            //return 2 if they are equal
+        }    
+    }
+}
+
+
+void divisor(int **num) {
+    int num_length = num[0][0];
+    for (int i = num_length; i > 0; i-- ) {
+        if (num[0][i] == 1) {
+            num[0][i] = 0;
+            num[0][i-1] = num[0][i-1] + 10;
+        }
+        else if (num[0][i] % 2 == 1) {
+            num[0][i] = num[0][i] / 2;
+            num[0][i-1] = num[0][i-1] + 10;
+        }
+        else {
+            num[0][i] = num[0][i] / 2;
+        }        
+    }
+}
+
 //multiplier
+
+
+
 
 
 /* void strToInt(char toDo[], int arr_len, int result[]){
@@ -167,6 +142,13 @@ int *strToInt(char toDo[])
         result[arr_len - i] = (int)(toDo[i] - '0');
     }
     return result;
+}
+
+char *get_num_in_str(void) {
+    char * str_num = (char *) malloc((MAX_LENGTH + 1) * sizeof(char));
+    printf("Please type in number \n");
+    scanf("%s", str_num);
+    return str_num;
 }
 
 void print_the_number(int toDo[])
