3eb2963003ea28bee6ab802d5aed6b6dbe4771aa
bHead_96 <r10525103@ntu.edu.tw>
2022-03-13 19:51:25 +0800

    Finish self_multiplier but still have free-memory issue
    I have finished the function, and make them work properly. There are two key reasons why
    I spent so many time on this function
    
    1. I didn't make the length of the result in the strToInt long enough to do calculation in the later
    section which resulted two differnt outputs on mac and linux. The reason why it's so important is
    that once you didn't make the length enough, you can't manipulate the number out of the original
    range. And it also made the output of adder_to_it_self wired, which casued by the reason above.
    
    2. In the one digit_multiplier, I forgot to add result[i] to temp, which might not be 0 since you
    you might have calculated a number which is more than 10 when i = i - 1.
    
    Working on it, but still need to take care of the memory free part after finish this question.

--- hw0/hw0_1.c
+++ hw0/hw0_1.c
@@ -6,23 +6,22 @@
 
 
 int comparator(int *, int *);
-int *strToInt(char *);
 void print_the_number(int *);
 void swap(int **, int **);
 void substractor(int **num1, int **num2);
 char *get_num_in_str();
 void divisor(int **);
-void adder_to_itself(int **num1, int **num2);
+void adder_to_itself_with_digit_control(int **num1, int **num2, int digit);
 int *one_digit_multiplier(int *, int);
+void self_multiplier(int **num1, int **num2);
+
 /*
 void strToInt(char toDo[], int arr_len, int result[])
-
 both of the ways are allowed, check out the difference
 you can whether put the arguments name or not
 szince char is different to char array, you should type char* instead of char in the first fashion
-
 */
-
+int *strToInt(char *);
 
 int main(void){
     
@@ -30,21 +29,22 @@ int main(void){
     char *num2 = get_num_in_str();    
     int *num1_transform = strToInt(num1);
     int *num2_transform = strToInt(num2);
-
-    int *tmp_1 = one_digit_multiplier(num1_transform, 3);
-    print_the_number(tmp_1);
-
-    adder_to_itself(&num1_transform, &num2_transform);
+    
+    self_multiplier(&num1_transform, &num2_transform);
     print_the_number(num1_transform);
     
-    int ans = 1;
+
+    //adder_to_itself_with_digit_control(&num1_transform, &num2_transform, 5);
+    
+    
+/*     int ans = 1;
     int tmp;
     while (num1_transform[0] != 0 && num2_transform[0] != 0) {
         
     }
 
     printf("%d\n", comparator(num1_transform, num2_transform));
-    substractor(&num1_transform, &num2_transform);
+    substractor(&num1_transform, &num2_transform); */
         
 }
 
@@ -127,43 +127,56 @@ void divisor(int **num) {
 }
 
 
-void adder_to_itself(int **num1, int **num2) {
-    int long_length = (num1[0][0] >= num2[0][0]) ? num1[0][0] : num2[0][0];
+void adder_to_itself_with_digit_control(int **num1, int **num2, int digit) {
     
+    /* if you wnat to add the number to the second digit, you just need to move the number to the left with 1 movement, which
+    is digit - 1 */
+    digit -= 1;
+
+    int long_length = (num1[0][0] >= num2[0][0]) ? num1[0][0] : num2[0][0];    
     //to check if the final result is longer than long_length
+
+    int final_length = (digit + num2[0][0] >= long_length) ? digit + num2[0][0] : long_length;
     
-    for (int i = 1; i <= long_length; i++) {
-        if ((num1[0][i] + num2[0][i]) >= 10) {            
-            num1[0][i+1] = num1[0][i+1] + 1;
+    
+    /* You need to run til the end of the number since the number you've got might be more than 10 on every digit which you have
+    to dealing with the i+1 digit */
+    for (int i = digit + 1; i <= final_length; i++) {
+
+        if ((num1[0][i] + num2[0][i-digit]) >= 10) {            
+            num1[0][i+1] += 1;
             //it's add 1 but not 10, bad mistake
 
-            num1[0][i] = (num1[0][i] + num2[0][i]) % 10;
+            num1[0][i] = (num1[0][i] + num2[0][i-digit]) % 10;
     
         }
         else {
-            num1[0][i] = num1[0][i] + num2[0][i];
+            num1[0][i] += num2[0][i-digit];
         }
     }
-    if (num1[0][long_length+1]) {
-        num1[0][0] = long_length + 1;
+    //if the number of the last digit is more than 10, you have to add 1 to i+1 digit
+    if (num1[0][final_length + 1]) {
+        num1[0][0] = final_length + 1;
     }
     else {
-        num1[0][0] = long_length;
+        num1[0][0] = final_length;
     }
 }
 
 int *one_digit_multiplier(int *multiplier, int multiplicand) {
     
-    int *result = (int *) malloc((multiplier[0] + 2) * sizeof(int));
+    //int *result = (int *) malloc((multiplier[0] + 2) * sizeof(int));
+    int *result = (int *) malloc((MAX_LENGTH + 1) * sizeof(int));
     
     //to check if the length of the result is more than multipler's
     int over = 0;
     int tmp = 0;
     for (int i = 1; i <= multiplier[0]; i++) {
         tmp = multiplier[i] * multiplicand;
-        if (tmp >= 10) {
-            result[i+1] = tmp / 10;
-            result[i] += tmp % 10;
+        //be careful, you need to add the temp to the number which was there to have % or /
+        if (tmp + result[i] >= 10) {
+            result[i+1] = (tmp +result[i]) / 10;
+            result[i] = (tmp +result[i]) % 10;
             over = 1;
         }
         else {
@@ -180,16 +193,30 @@ int *one_digit_multiplier(int *multiplier, int multiplicand) {
 }
 
 
-void multiplier(int **num1, int time) {  
+ //the result will be like num1 = num1 * num2
+void self_multiplier(int **num1, int **num2) {
+    int *result = (int *) malloc((MAX_LENGTH + 1) * sizeof(int));
+    result[0] = 0;
 
-}
+    for (int i = 1; i <= num2[0][0]; i++) {
+        //get the result of i digit of num2 multiple num1
+        int *tmp = one_digit_multiplier(* num1, num2[0][i]);
+        //print_the_number(tmp);
+        adder_to_itself_with_digit_control(&result, &tmp, i);                
+    }
+    *num1 = result;
+    //print_the_number(result);
+} 
 
 
 int *strToInt(char toDo[])
-{
-    int arr_len = strlen(toDo);
+{   
+
     //store the length of the long integer in the first element of the array
-    int * result = (int *) malloc((arr_len + 1) * sizeof(int));
+    int arr_len = strlen(toDo);
+
+    //the reason why the the result of linux and mac are different is because I malloc arr_length+1 instead of MAX_LENGTH+1 in the beginning
+    int * result = (int *) malloc((MAX_LENGTH + 1) * sizeof(int));
     result[0] = arr_len;
     for (int i = 0; i < arr_len; i++){
         result[arr_len - i] = (int)(toDo[i] - '0');
@@ -198,7 +225,10 @@ int *strToInt(char toDo[])
 }
 
 char *get_num_in_str(void) {
+    
     char * str_num = (char *) malloc((MAX_LENGTH + 1) * sizeof(char));
+
+
     printf("Please type in number \n");
     scanf("%s", str_num);
     return str_num;
