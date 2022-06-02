3b192e000b4d77e5afbd8d16f488dd642b4e480b
bHead_96 <chinghsiang2019@gmail.com>
2022-06-02 12:03:08 +0800

    HW4_3: Add boom but still got some run time error

--- hw4/hw4_3.c
+++ hw4/hw4_3.c
@@ -55,32 +55,71 @@ void group(long long index_1, long long index_2, DisjointSet *ds) {
 }
 
 int main(void) {
-    long long shops;
+    //shops is used to count the shops left
+    long long shops;    
     long long collections;
     scanf("%lld", &shops);
     scanf("%lld", &collections);
-    DisjointSet ds[shops+1];
+    //total_shops is used to record the number of shops at the begining
+    long long total_shops = shops;
+    DisjointSet ds[total_shops+1];
     long long times_of_querys = 0;
 
-    char *job = (char *)malloc(sizeof(char) * 6);
+    char *job = (char *)malloc(sizeof(char) * 6);    
     long long *answer = (long long*)malloc(sizeof(long long) * collections);
     long long data_1;
     long long data_2;
-    makeset(shops, ds);
-
-    for (long long i = 0; i < collections; i++) {
+    //jump_to is used to record the value of boom
+    long long jump_to;
+    DisjointSet *history_for_boom = (DisjointSet*)malloc(sizeof(DisjointSet) * (collections+1) * (total_shops+1));
+    makeset(total_shops, ds);
+    //Initialize for the first row of the history
+    //Use rank to store the number of store
+    history_for_boom[0].rank = shops;
+    for (long long i = 1; i <= shops; i++) {
+        history_for_boom[i].parent = i;
+        history_for_boom[i].rank = 0;
+    }
+    
+    for (long long i = 1; i <= collections; i++) {
         scanf("%s", job);
         if (!strcmp(job, "query")) {
             times_of_querys++;            
             //printf("%llu\n", shops);
             answer[times_of_querys-1] = shops;
+            
+            history_for_boom[i * (total_shops+1)].rank = shops;
+            for (long long j = 1; j <= total_shops; j++) {
+                history_for_boom[i * (total_shops+1)+j].parent = ds[j].parent;
+                history_for_boom[i * (total_shops+1)+j].rank = ds[j].rank;
+            }
         }
-        else {        
+        else if (!strcmp(job, "merge")) {        
             scanf("%lld", &data_1);
             scanf("%lld", &data_2);
-            if (!same_set(data_1, data_2, ds)){
-                group(data_1, data_2, ds);
+            if (!same_set(data_1, data_2, ds)) {
+                group(data_1, data_2, ds);                
                 shops--;
+            }
+            //Shops left in each round
+            history_for_boom[i * (total_shops+1)].rank = shops;
+            for (long long j = 1; j <= total_shops; j++) {
+                history_for_boom[i * (total_shops+1)+j].parent = ds[j].parent;
+                history_for_boom[i * (total_shops+1)+j].rank = ds[j].rank;
+            }
+        }
+        else {
+            scanf("%lld", &jump_to);
+            for (long long j = 1; j <= total_shops; j++) {
+                ds[j].parent = history_for_boom[jump_to * (total_shops+1)+j].parent;                
+                ds[j].rank = history_for_boom[jump_to * (total_shops+1)+j].rank;                
+                shops = history_for_boom[jump_to * (total_shops+1)].rank;
+                //printf("%lld\n\n", ds[j].parent);
+                //printf("%lld\n\n", ds[j].rank);
+                //printf("%lld\n\n", shops);
+                history_for_boom[i * (total_shops+1)].rank = shops;
+                history_for_boom[i * (total_shops+1)+j].parent = ds[j].parent;
+                history_for_boom[i * (total_shops+1)+j].rank = ds[j].rank;
             }
         }
     }        
